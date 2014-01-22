ebe332a557829fd342b5ae3e1e0a4cd1105043d2
Matt Antonio <mantonio@fullsail.edu>
2014-01-22 15:20:29 -0500

    working on registration

---
+++ app/Controller/RegistrationsController.php
@@ -0,0 +1,25 @@
+<?php 
+
+class RegistrationsController extends AppController {
+
+	public function register() {
+
+		if(!empty($this->params['form'])) {
+
+			if($this->User->save($this->params('form'))) {
+
+				$this->Session->setFlash(__('Registration Successful!'));
+				return $this->redirect(array('action' => 'login'));
+
+			}else {
+
+				$this->Session->setFlash(__('Registration Was Not Successful. Please Try Again!'));
+				return $this->redirect(array('action' => 'register'));
+
+			}
+
+		}
+
+	}
+	
+}
