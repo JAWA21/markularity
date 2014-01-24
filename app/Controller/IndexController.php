a7b1f137cd83022368b8a660f8489ce121bb1d36
Paul Burge <paul.burge8@gmail.com>
2014-01-24 16:32:12 -0500

    added jquery

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -1,7 +1,7 @@
 <?
 class IndexController extends AppController{
 
-    public $helpers = array('Html', 'Form','Js' => array('Jquery'));
+    public $helpers = array('Html', 'Form');
 
     public function index() {
     	$this->layout = 'index';
